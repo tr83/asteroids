4e1b12a4724f6fc54ed09f98399bafea7fe3874b
tr83 <terorii@gmail.com>
2024-12-04 18:46:28 +0200

    Fps limit to 60

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -20,6 +22,8 @@ def main():
             
         screen.fill(000000)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":

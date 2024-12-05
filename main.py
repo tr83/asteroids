78b8d9e8ac13634b65deed2446772be3f4c0fcfc
tr83 <terorii@gmail.com>
2024-12-05 19:15:29 +0200

    Add player movement Add groups

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -23,7 +26,12 @@ def main():
                 return
             
         screen.fill(000000)
-        player.draw(screen)
+        
+        for item in updatable:
+            item.update(dt)
+
+        for item in updatable:
+            item.draw(screen)
         
         pygame.display.flip()
         dt = clock.tick(60) / 1000
